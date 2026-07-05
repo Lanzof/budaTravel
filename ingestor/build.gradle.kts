5f0b8f8f1fa615dcd30c38458b1b8a44f2949c54
Bender AI <bender-ai@users.noreply.github.com>
2026-07-06 00:08:24 +0400

    Add Flyway PostgreSQL database support

--- ingestor/build.gradle.kts
+++ ingestor/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
     testImplementation(kotlin("test"))
